8377aac6d0d5b1ad4560da3a0e36394024a66c7f
daehanlee <daehan@acriil.com>
2020-05-16 17:11:16 +0900

    test: change title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          github pages - deploy
+          github actions
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ReactJs 
         </a>
       </header>
     </div>
